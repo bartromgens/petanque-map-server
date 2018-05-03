69db796b6b99817ef8e281dc4bcce22b93599095
Bart Römgens <bart.romgens@gmail.com>
2018-05-03 22:22:12 +0200

    Add court admin

--- courts/admin.py
+++ courts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from courts.models import Court
+
+class CourtAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'osm_id',
+        'osm_type',
+        'osm_url',
+        'lon',
+        'lat',
+    )
+
+admin.site.register(Court, CourtAdmin)
